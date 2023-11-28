de1ea2281c006388cbfcf65c9854912fe1f409fd
nu-jey <yp50777@naver.com>
2023-11-28 23:51:20 +0900

    MVVM+Combine: 회원가입 예제 클로닝

--- MVVM+Combine/View(controller)/ViewController.swift
+++ MVVM+Combine/View(controller)/ViewController.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-
+import Combine
 class ViewController: UIViewController {
     // 요구사항
     // 1. id는 5자리 이상
@@ -17,11 +17,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var idTextField: UITextField!
     @IBOutlet weak var pwdTextField: UITextField!
     @IBOutlet weak var confirmTextField: UITextField!
-    
     @IBOutlet weak var btnSignup: UIButton!
+    
+    private var viewModel:LoginViewModel = LoginViewModel()
+    private var cancellables = Set<AnyCancellable>()
     override func viewDidLoad() {
         super.viewDidLoad()
+        setup()
+        bind()
+    }
+    
+    func setup() {
         btnSignup.isEnabled = false
+    }
+    
+    func bind() {
+        let input = LoginViewModel.Input(id: idTextField.textPublisher, pwd: pwdTextField.textPublisher, confirm: confirmTextField.textPublisher)
+        let ouput = viewModel.transform(input)
+        ouput.btnState
+            .receive(on: DispatchQueue.main)
+            .sink(receiveValue: { state in
+                self.btnSignup.isEnabled = state
+            })
+            .store(in: &cancellables)
     }
 
     @IBAction func pressedBtnSignup(_ sender: Any) {
--- MVVM+Combine/ViewModel/ViewModel.swift
+++ MVVM+Combine/ViewModel/ViewModel.swift
@@ -6,7 +6,87 @@
 //
 
 import Foundation
-
-class ViewModel {
+import Combine
+import UIKit
+protocol ViewModel {
+    associatedtype Input
+    associatedtype Output
+    func transform(_ input: Input) -> Output
+}
+class LoginViewModel: ViewModel {
+    struct Input {
+        let id: AnyPublisher<String, Never>
+        let pwd: AnyPublisher<String, Never>
+        let confirm: AnyPublisher<String, Never>
+    }
     
+    struct Output {
+        let btnState: AnyPublisher<Bool, Never>
+    }
+    func transform(_ input: Input) -> Output {
+        let getBtnState = input.id
+            .combineLatest(input.pwd, input.confirm)
+            .map { id, pwd, confirm in
+                id.count >= 5 && pwd.count >= 7 && (pwd == confirm)
+            }
+            .eraseToAnyPublisher()
+        return Output(btnState: getBtnState)
+    }
+}
+
+
+extension UIControl {
+    func controlPublisher(for event: UIControl.Event) -> UIControl.EventPublisher {
+        return UIControl.EventPublisher(control: self, event: event)
+      }
+    
+    // Publisher
+    struct EventPublisher: Publisher {
+        typealias Output = UIControl
+        typealias Failure = Never
+        
+        let control: UIControl
+        let event: UIControl.Event
+        
+        func receive<S>(subscriber: S) where S : Subscriber, Never == S.Failure, UIControl == S.Input {
+            let subscription = EventSubscription(control: control, subscrier: subscriber, event: event)
+            subscriber.receive(subscription: subscription)
+        }
+    }
+    
+    // Subscription
+    fileprivate class EventSubscription<EventSubscriber: Subscriber>: Subscription where EventSubscriber.Input == UIControl, EventSubscriber.Failure == Never {
+
+        let control: UIControl
+        let event: UIControl.Event
+        var subscriber: EventSubscriber?
+        
+        init(control: UIControl, subscrier: EventSubscriber, event: UIControl.Event) {
+            self.control = control
+            self.subscriber = subscrier
+            self.event = event
+            
+            control.addTarget(self, action: #selector(eventDidOccur), for: event)
+        }
+        
+        func request(_ demand: Subscribers.Demand) {}
+        
+        func cancel() {
+            subscriber = nil
+            control.removeTarget(self, action: #selector(eventDidOccur), for: event)
+        }
+        
+        @objc func eventDidOccur() {
+            _ = subscriber?.receive(control)
+        }
+    }
+}
+
+extension UITextField {
+    var textPublisher: AnyPublisher<String, Never> {
+        controlPublisher(for: .editingChanged)
+            .map { $0 as! UITextField }
+            .map { $0.text! }
+            .eraseToAnyPublisher()
+    }
 }
